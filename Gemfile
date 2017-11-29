a44a203e4c4dbbd86a5511c5713b2d95a4f77ffc
MVONDO Yannick <mvondoyannick@gmail.com>
2017-11-29 07:40:51 +0100

    Devise configuration, add signin & signup view and methods

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
-#gem 'sqlite3'
+gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -25,7 +25,7 @@ gem 'carrierwave', '~> 1.2', '>= 1.2.1'
 
 gem 'omniauth', '~> 1.7', '>= 1.7.1'
 
-gem 'pg', '~> 0.21.0'
+#gem 'pg', '~> 0.21.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Mailer
+  config.action_mailer.devery_method = :smtp
+
+  #pour l'utilisation de maildev (a installer)
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
+  config.action_mailer.default_url_options = {host: 'localhost', post: 3000}
 end
