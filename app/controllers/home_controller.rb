78c1eed486a87adcacb338f22d8e5942c8592a69
MVONDO Yannick <mvondoyannick@gmail.com>
2017-11-30 19:38:30 +0100

    update login views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   	if user_signed_in?
-  		@current_user = current_user
+  		redirect_to dashboard_index_path
   	else
   		@current_user = "Une erreur est survenue"
   	end
